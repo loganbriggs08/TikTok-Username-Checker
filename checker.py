ce17b52aa1558f340ae2666716286352dacf38c5
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-27 19:53:50 +0100

    check username, print it and add to file

--- checker.py
+++ checker.py
@@ -1,5 +1,8 @@
+from colorama import *
 from helpers.requests import username_available
 from helpers.files import create_dir, create_file, add_username
+
+init(convert=True)
 
 class Checker: 
     def __init__(self) -> None:
@@ -25,3 +28,24 @@ class Checker:
         
 if __name__ == "__main__":
     Checker.setup()
+    
+    try:
+        with open("file.txt", "r") as f:
+            lines = f.readlines()
+            if not lines:
+                raise Exception(f"{Fore.RED}[ERROR]{Fore.RESET} File is empty, please add some usernames.")
+            else:
+                for line in lines:
+                    if line.strip():
+                        result = Checker.check_username(line.strip())
+                        
+                        if result == True:
+                            print(f"{Fore.GREEN} https://www.tiktok.com/@{line.strip()}")
+                            Checker.add_username("./data", "available", line.strip())
+                            
+                        elif result == False:
+                            print(f"{Fore.RED} https://www.tiktok.com/@{line.strip()}")
+                            Checker.add_username("./data", "unavailable", line.strip())
+                            
+    except FileNotFoundError:
+        print(f"{Fore.RED}[ERROR]{Fore.RESET} Please create a usernames.txt file.")
