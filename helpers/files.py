ca52e955efb6abb81a79db999aa1886aa86994f7
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-27 19:45:25 +0100

    add_username function created to add usernames to files.

--- helpers/files.py
+++ helpers/files.py
@@ -1,5 +1,7 @@
 import os 
 
+URL_Base: str = "https://www.tiktok.com"
+
 def create_dir(directory_name: str) -> bool: 
     if os.path.exists(f"./{directory_name}"):
         return False
@@ -17,4 +19,8 @@ def create_file(dir_name: str, file_name: str) -> bool:
             return True
     else:
         return False
-    
+
+def add_username(dir_name: str, file_name: str, username: str):
+    with open(f"./{dir_name}/{file_name}.txt", 'w') as f:
+        f.write(f"\n{URL_Base}/@{username}")
+        f.close()
