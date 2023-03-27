561a14c1b2b05adeb82b52e35bc8c0e348e1714d
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-27 20:05:39 +0100

    error message added for unreachable usernames

--- helpers/requests.py
+++ helpers/requests.py
@@ -1,13 +1,15 @@
-import httpx 
+import httpx
+
+from colorama import * 
 
 URL_Base: str = "https://www.tiktok.com"
 
 def username_available(username: str) -> bool: 
     response = httpx.get(f"{URL_Base}/@{username}")
-    
+        
     if response.status_code == 200:
-        return False
+        return True
     elif response.status_code == 404:
         return False
     else:
-        return False
+        print(f"{Fore.RED}[ERROR]{Fore.RESET} Couldn't fetch username.")
