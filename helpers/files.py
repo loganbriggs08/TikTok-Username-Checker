eca8678b31bfa06339174d968e1e2b3954336987
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-27 19:34:42 +0100

    added create_file function

--- helpers/files.py
+++ helpers/files.py
@@ -9,4 +9,12 @@ def create_dir(directory_name: str) -> bool:
             return True
         except:
             return False
+        
+def create_file(dir_name: str, file_name: str) -> bool: 
+    if os.path.exists(f"./{dir_name}"):
+        with open(f"./{dir_name}/{file_name}.txt", 'w') as f:
+            f.close()
+            return True
+    else:
+        return False
     
